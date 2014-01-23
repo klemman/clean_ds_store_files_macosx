d12a50325b179ab60b9bb1eae664662c1a2016bc
clement.capiaux@valiantys.com <ganjaweed8>
2014-01-23 21:41:06 +0100

    Cleaned up and improved sh script logging messages

--- cleanDsStore.sh
+++ cleanDsStore.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-TODAY=$(date +%d/%m/%y_%r)
-echo $TODAY
 
-#${DATE} >> /Users/klemman/test.txt
+TODAY=$(date +%d/%m/%y_%r)
+echo '\n'
+echo '-- BEGIN JOB EXECUTION : '$TODAY' --'
+
+# perform '.DS_Store' files removal on entire FileSystem
 find / -name ".DS_Store" -print -depth -exec rm {} \;
